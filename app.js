e639f19f37fd318c745d96188d87cad1906acfa8
Hannah Stover <hrosestover@gmail.com>
2020-01-06 12:57:22 -0800

    Express Generator REST API

--- app.js
+++ app.js
@@ -6,8 +6,16 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+let dishRouter = require('./routes/dishRouter');
+let promoRouter = require('./routes/promoRouter');
+let leaderRouter = require('./routes/leaderRouter');
 
 var app = express();
+
+// mount route handlers
+app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
