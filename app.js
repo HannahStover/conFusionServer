5bf800e695e7475799542dc67f49ae4aaf9a496d
Hannah Stover <hrosestover@gmail.com>
2020-01-09 14:26:42 -0800

    Express REST API with MongoDB and Mongoose Part 1

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-let dishRouter = require('./routes/dishRouter');
-let promoRouter = require('./routes/promoRouter');
-let leaderRouter = require('./routes/leaderRouter');
+const Dishes = require('./models/dishes');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 
-var app = express();
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+connect.then(
+  db => {
+    console.log('Connected correctly to server');
+  },
+  err => {
+    console.log(err);
+  }
+);
+
+const app = express();
 
 // mount route handlers
 app.use('/dishes', dishRouter);
