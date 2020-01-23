46c319b524f2425ef492b7853b931fbc0fec2a45
Hannah Stover <hrosestover@gmail.com>
2020-01-23 13:26:04 -0800

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: '12345-67890-09876-54321',
-  mongoUrl: 'mongodb://localhost:27017/conFusion'
+  mongoUrl: 'mongodb://localhost:27017/conFusion',
+  facebook: {
+    clientId: '196610198128222',
+    clientSecret: '5de7cc20d1c0c6cf310ca1c1c9a32617'
+  }
 };
